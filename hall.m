077100532a06c2a4dfc010b2927934e87be3479a
Paul Anderson <paul.anderson2@wpafb.af.mil>
2011-01-11 11:26:33 -0500

    Copied some of the SBtoolbox functionality for importing experimental data.
    Woolard data for sbtoolbox.xls:
    * Added experimental data in sbtoolbox format.
    
    hall.m:
    * Added SIMULATION constant that is set to 1 for Hall and 2 for Woolard.
    
    immune_system.csl:
    * Made changes to the simulation, so we can switch between Hall and Woolard.
    
    Added Woolard files.

--- hall.m
+++ hall.m
@@ -1,6 +1,7 @@
 prepare @clear @all
 
-TSTOP=72
+SIMULATION = 1; % Hall
+TSTOP=72;
 start @nocallback
-results = [_t _m _dc _nk _mono _monoblood _rhop _n _pn _np _ic _aic _dca]
+results = [_t _m _dc _nk _mono _monoblood _rhop _n _pn _np _ic _aic _dca];
 save results @file='hall_simulation_results' @format=Matlab
