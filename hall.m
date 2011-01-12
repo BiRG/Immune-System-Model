b34f55cababa7e8cd6cc6967d985a6574d7e80f5
Paul Anderson <paul.anderson2@wpafb.af.mil>
2011-01-12 10:14:05 -0500

    hall.m: * Added IFNG to results table
    immune_system.csl
    * Added and modified code to now keep track of infected macrophages.
    * The infected macrophages are used to generate interferon gamma (started but not finished).

--- hall.m
+++ hall.m
@@ -3,5 +3,5 @@ prepare @clear @all
 SIMULATION = 1; % Hall
 TSTOP=72;
 start @nocallback
-results = [_t _m _dc _nk _mono _monoblood _rhop _n _pn _np _ic _aic _dca];
+results = [_t _m _dc _nk _mono _monoblood _rhop _n _pn _np _ic _aic _dca _mp _ifng];
 save results @file='hall_simulation_results' @format=Matlab
