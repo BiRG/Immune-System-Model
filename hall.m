06c2d6a546e9605053f6570bae2501b83c9b712f
Paul Anderson <paul.anderson2@wpafb.af.mil>
2011-01-18 15:19:30 -0500

    immune_system.csl: * Added first attempt at cytokines

--- hall.m
+++ hall.m
@@ -1,21 +1,7 @@
 prepare @clear @all
 
-% mouse values
-mouse
-% U112 values
-U112
-
-% put initial cell numbers hall.m, or in mouse m-file?
-M0 = 1.75e6
-DC0 = 4.5e5 
-NK0 = 340
-N0 = 6e5
-MONO0 = 5e5
-%MonoBlood0 = Mono0
-%MonoBloodTissue0 = Mono0
-
 SIMULATION = 1; % Hall
 TSTOP=72;
 start @nocallback
-results = [_t _m _dc _nk _mono _monoblood _rhop _n _pn _np _ic _aic _dca _mp _ifng];
+results = [_t _m _dc _nk _mono _monoblood _rhop _n _pn _np _ic _aic _dca _mp _ifng _il1a _il12];
 save results @file='hall_simulation_results' @format=Matlab
